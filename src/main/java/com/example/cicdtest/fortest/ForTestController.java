8e192c333c4456376b9fa80fb14ca50a8157de54
gkswn <ahj304819@gmail.com>
2026-02-25 00:11:01 +0900

    fix error

--- src/main/java/com/example/cicdtest/fortest/ForTestController.java
+++ src/main/java/com/example/cicdtest/fortest/ForTestController.java
@@ -1,7 +1,9 @@
 package com.example.cicdtest.fortest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ForTestController {
 	@GetMapping("/hello")
 	public String hello() {
